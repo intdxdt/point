6cb8be2e933738e98fe582c129bb5f0a76d0eec0
Titus Tienaah <titus.intdxdt@gmail.com>
2019-11-10 15:23:18 +0000

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,149 +1,217 @@
 use coordinate::Coordinate;
 use std::fmt::Debug;
-use math_util::{Feq, Flt, const_tau, const_pi, EPSILON};
+use math_util::{num, Feq, EPSILON, PI, TAU, NumCast};
 use std::ops::{Index, IndexMut};
+use robust_orientation::orientation_2d;
 
 /// Point is a 2D (x:float, y:float) point type.
 /// float : f32 & f64 - required for most computations
 /// requiring area, distance, trigonometry, etc.
 #[derive(Copy, Clone, PartialOrd, Debug)]
-pub struct Point<T>
-    where T: Flt {
-    pub x: T,
-    pub y: T,
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
 }
 
-impl<T> Point<T>
-    where T: Flt {
-    pub fn new(x: T, y: T) -> Point<T> {
+impl Point {
+    pub fn new(x: f64, y: f64) -> Point {
         Point { x, y }
     }
-    pub fn new_from_array(a: &[T; 2]) -> Point<T> {
+
+    pub fn new_from_array(a: &[f64; 2]) -> Point {
         Point { x: a[0], y: a[1] }
     }
 
     ///Operator : equals
-    pub fn equals(&self, other: &Point<T>) -> bool {
+    pub fn equals(&self, other: &Point) -> bool {
         self.x.feq(other.x) && self.y.feq(other.y)
     }
 
     ///As array
-    pub fn as_array(&self) -> [T; 2] {
+    #[inline]
+    pub fn as_array(&self) -> [f64; 2] {
         [self.x, self.y]
     }
 
     ///As tuple
-    pub fn as_tuple(&self) -> (T, T) {
+    pub fn as_tuple(&self) -> (f64, f64) {
         (self.x, self.y)
     }
 
     ///Computes vector magnitude given x an dy component
-    pub fn magnitude(&self) -> T {
+    pub fn magnitude(&self) -> f64 {
         self.x.hypot(self.y)
+    }
+    ///Computes vector square magnitude given pt as x, y components
+    pub fn square_magnitude(&self) -> f64 {
+        (self.x * self.x) + (self.y * self.y)
+    }
+
+    ///Computes distance between two points
+    pub fn distance(&self, other: &Self) -> f64 {
+        self.sub(other).magnitude()
     }
 
     ///Point from magnitude and direction
-    pub fn component(m: T, d: T) -> Point<T> {
+    pub fn component(m: f64, d: f64) -> Point {
         Point::new(m * d.cos(), m * d.sin())
     }
 
     ///Dot Product of two points as vectors
-    pub fn dot_product(&self, o: Point<T>) -> T {
+    pub fn dot_product(&self, o: Point) -> f64 {
         (self.x * o.x) + (self.y * o.y)
     }
 
     ///2D cross product of AB and AC vectors,
     ///i.e. z-component of their 3D cross product.
     ///negative cw and positive if ccw
-    pub fn cross_product(&self, b: Point<T>) -> T {
+    pub fn cross_product(&self, b: Point) -> f64 {
         return (self.x * b.y) - (self.y * b.x);
     }
 
     ///Deflect_vector computes vector deflection given deflection angle and
     /// side of vector to deflect from (from_end)
-    pub fn deflect(&self, mag: T, defl_angle: T, from_end: bool) -> Point<T> {
-        return self.extend(mag, T::PI() - defl_angle, from_end);
+    pub fn deflect(&self, mag: f64, defl_angle: f64, from_end: bool) -> Point {
+        return self.extend(mag, PI - defl_angle, from_end);
     }
 
     ///kproduct scales x and y components by constant  k
-    pub fn kproduct(&self, k: T) -> Point<T> {
+    pub fn kproduct(&self, k: f64) -> Point {
         self.mult(k)
     }
 
+    ///negative of point
+    pub fn neg(&self) -> Point {
+        self.kproduct(-1.0)
+    }
+
     ///Dir computes direction in radians - counter clockwise from x-axis.
-    pub fn direction(&self) -> T {
+    pub fn direction(&self) -> f64 {
         let mut d = self.y.atan2(self.x);
-        if d < T::zero() {
-            d += const_tau()
+        if d < 0.0 {
+            d += TAU
         }
         return d;
     }
 
     ///Revdir computes the reversed direction from a foward direction
-    pub fn reverse_direction(d: T) -> T {
-        let pi = T::PI();
-        let mut r = d - pi;
-        if d < pi {
-            r = d + pi;
+    pub fn reverse_direction(d: f64) -> f64 {
+        let mut r = d - PI;
+        if d < PI {
+            r = d + PI;
         }
         return r;
     }
 
     ///deflection angle
-    pub fn deflection_angle(bearing1: T, bearing2: T) -> T {
+    pub fn deflection_angle(bearing1: f64, bearing2: f64) -> f64 {
         let mut a = bearing2 - Point::reverse_direction(bearing1);
-        if a < T::zero() {
-            a = a + const_tau();
+        if a < 0.0 {
+            a += TAU;
         }
-        return T::PI() - a;
+        return PI - a;
     }
 
 
     ///Unit vector of point
-    pub fn unit_vector(&self) -> Point<T> {
+    pub fn unit_vector(&self) -> Point {
         let mut m = self.magnitude();
-        if m.feq(T::zero()) {
-            m = T::from(EPSILON).unwrap();
+        if m.feq(0.0) {
+            m = EPSILON;
         }
         Point::new(self.x / m, self.y / m)
     }
 
     ///Projects self on to v
-    pub fn project(&self, v: Point<T>) -> T {
+    pub fn project(&self, v: Point) -> f64 {
         return self.dot_product(v.unit_vector());
     }
 
-    ///2D cross product of AB and AC vectors given A, B, and Pnts as points,
+    ///2D cross product of AB and AC vectors given A, B, and C as points,
     ///i.e. z-component of their 3D cross product.
     ///Returns a positive value, if ABC makes a counter-clockwise turn,
     ///negative for clockwise turn, and zero if the points are collinear.
-//    fn orientation2d(a:Point<T>, b:Point<T>, c:Point<T>) -> T{
-//        return robust.Orientation2D(a[0..2], b[0..2], c[0..2])
-//    }
+    pub fn orientation2d(&self, a: Point, b: Point) -> f64 {
+        return orientation_2d(&a.as_array(), &b.as_array(), &self.as_array());
+    }
 
     ///Extends vector from the end or beginning based on `from_end`.
-    pub fn extend(&self, magnitude: T, angle: T, from_end: bool) -> Point<T> {
+    pub fn extend(&self, magnitude: f64, angle: f64, from_end: bool) -> Point {
         //from a of v back direction initiates as fwd v direction anticlockwise
         //bb - back bearing
         //fb - forward bearing
         let mut bb = self.direction();
         if from_end {
-            bb += const_pi();
+            bb += PI;
         }
         let mut fb = bb + angle;
-        let tau: T = const_tau();
-        if fb > tau {
-            fb -= tau;
+        if fb > TAU {
+            fb -= TAU;
         }
         return Point::component(magnitude, fb);
     }
+
+    ///Distance from segment end points to self
+    pub fn distance_to_point(&self, sa: Point, sb: Point) -> f64 {
+        self.distance_to_pt(sa, sb, f64::hypot)
+    }
+
+    ///Distance from segment end points to self
+    pub fn distance_to_pt(&self, sa: Point, sb: Point, hypot_func: fn(f64, f64) -> f64) -> f64 {
+        let (ax, ay) = (sa.x, sa.y);
+        let (bx, by) = (sb.x, sb.y);
+        let (px, py) = (self.x, self.y);
+        let (dx, dy) = (bx - ax, by - ay);
+        let isz_x = dx.feq(0.0);
+        let isz_y = dy.feq(0.0);
+
+        if isz_x && isz_y {
+            //line with zero length
+            hypot_func(px - ax, py - ay)
+        } else {
+            let u = (((px - ax) * dx) + ((py - ay) * dy)) / (dx * dx + dy * dy);
+            let (c_ptx, c_pty) =
+                if u < 0.0 {
+                    (ax, ay)
+                } else if u > 1.0 {
+                    (bx, by)
+                } else {
+                    (ax + u * dx, ay + u * dy)
+                };
+
+            hypot_func(px - c_ptx, py - c_pty)
+        }
+    }
 }
 
-impl<T> Eq for Point<T>
-    where T: Flt {}
+impl<T> From<(T, T)> for Point where T: NumCast + Copy {
+    fn from(tup: (T, T)) -> Self {
+        Point { x: num::cast(tup.0).unwrap(), y: num::cast(tup.1).unwrap() }
+    }
+}
 
-impl<T> PartialEq for Point<T>
-    where T: Flt {
+impl<T> From<[T; 2]> for Point where T: NumCast + Copy {
+    fn from(array: [T; 2]) -> Self {
+        Point { x: num::cast(array[0]).unwrap(), y: num::cast(array[1]).unwrap() }
+    }
+}
+
+impl From<&[f64]> for Point {
+    fn from(slice: &[f64]) -> Self {
+        Point { x: slice[0], y: slice[1] }
+    }
+}
+
+impl From<&Vec<f64>> for Point {
+    fn from(vec: &Vec<f64>) -> Self {
+        Point { x: vec[0], y: vec[1] }
+    }
+}
+
+
+impl Eq for Point {}
+
+impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
         self.equals(other)
     }
@@ -153,9 +221,8 @@ impl<T> PartialEq for Point<T>
     }
 }
 
-impl<T> Coordinate for Point<T> where
-    T: Flt {
-    type Scalar = T;
+impl Coordinate for Point {
+    type Scalar = f64;
     const DIM: usize = 2;
 
     fn gen(dim_val: impl Fn(usize) -> Self::Scalar) -> Self {
@@ -182,8 +249,8 @@ impl<T> Coordinate for Point<T> where
     }
 }
 
-impl<T> Index<usize> for Point<T> where T: Flt {
-    type Output = T;
+impl Index<usize> for Point {
+    type Output = f64;
     fn index(&self, i: usize) -> &Self::Output {
         match i {
             0 => &self.x,
@@ -193,41 +260,21 @@ impl<T> Index<usize> for Point<T> where T: Flt {
     }
 }
 
-impl<T> IndexMut<usize> for Point<T> where T: Flt {
+impl IndexMut<usize> for Point {
     fn index_mut(&mut self, i: usize) -> &mut Self::Output {
         self.nth_mut(i)
     }
 }
 
+#[macro_export]
+macro_rules! pt {
+    ($l :expr, $r:expr) => {
+        Point{x : ($l) as f64, y : ($r) as f64}
+    };
+    [$l :expr, $r:expr] => {
+        Point{x : ($l) as f64, y : ($r) as f64}
+    };
+}
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_point() {
-        let pa = Point::new_from_array(&[3.0, 4.0]);
-        let mut m_pa = Point::new_from_array(&[3.0, 4.0]);
-        let pb = Point::new(3.0, 4.0);
-        let pc = Point::new(5.0, 4.0);
-
-        assert_eq!(pa.as_tuple(), (3., 4.));
-        assert_eq!(pa.as_array(), [3.0, 4.0]);
-        assert_eq!((pa[0], pa[1]), (3., 4.));
-        assert_eq!((pa.nth(0), pa.nth(1)), (3., 4.));
-        assert_eq!((m_pa.nth(0), m_pa.nth(1)), (3., 4.));
-        m_pa[0] = 0.;
-        m_pa[1] = 5.;
-        assert_eq!((m_pa[0], m_pa[1]), (0., 5.));
-        assert!(m_pa.square_length().feq(25.0));
-
-        assert_eq!(pa, pb);
-        assert_ne!(pa, pc);
-        assert_ne!(pb, pc);
-        assert!(pb != pc);
-        assert!(pa.equals(&pb));
-
-        let cb = pb.comp(&pc);
-        assert_eq!(cb.as_tuple(), (-2.0, 0.0));
-    }
-}
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,272 @@
+use super::*;
+use math_util::{round, Feq, SQRT_2, FRAC_PI_4};
+
+#[test]
+fn test_point() {
+    let pa = pt![3, 4];
+    let mut m_pa = Point::new_from_array(&[3.0, 4.0]);
+    let pb = Point::new(3.0, 4.0);
+    let pc = Point::new(5.0, 4.0);
+
+    assert_eq!(pa.as_tuple(), (3., 4.));
+    assert_eq!(pa.as_array(), [3.0, 4.0]);
+    assert_eq!((pa[0], pa[1]), (3., 4.));
+    assert_eq!((pa.nth(0), pa.nth(1)), (3., 4.));
+    assert_eq!((m_pa.nth(0), m_pa.nth(1)), (3., 4.));
+    m_pa[0] = 0.;
+    m_pa[1] = 5.;
+    assert_eq!((m_pa[0], m_pa[1]), (0., 5.));
+    assert!(m_pa.square_length().feq(25.0));
+
+    assert_eq!(pa, pb);
+    assert_ne!(pa, pc);
+    assert_ne!(pb, pc);
+    assert!(pb != pc);
+    assert!(pa.equals(&pb));
+
+    let cb = pb.comp(&pc);
+    assert_eq!(cb.as_tuple(), (-2.0, 0.0));
+}
+
+#[test]
+fn test_distance_magnitude() {
+    let pt: Point = [3., 0.].into();
+    assert_eq!(pt.distance(&Point { x: 0., y: 4. }), 5.0);
+    assert_eq!(pt.square_distance(&Point { x: 0., y: 4. }), 25.0);
+    let pt: Point = [3., 4.].into();
+    assert_eq!(pt.distance(&Point { x: 4., y: 5. }), SQRT_2);
+    assert_eq!(pt.square_distance(&Point { x: 4., y: 5. }), 2.0);
+
+    let a = pt![0, 0];
+    let b = pt![4, 3];
+    let z = pt![0, 0];
+    let o = pt![1, 1];
+    assert_eq!(o.distance(&z), SQRT_2);
+    let x = pt!(-3, 2);
+    assert_eq!(round(x.distance(&z), 8),
+               round(3.605551275463989, 8));
+
+    assert_eq!(pt!(3, 4).magnitude(), 5.0);
+    let x = pt!(3, 4);
+    assert_eq!(x.distance(&z), 5.0);
+    assert_eq!(a.distance(&b), 5.0);
+    let x = pt!(3, 4);
+    assert_eq!(x.square_distance(&z), 25.0);
+    assert_eq!(pt!(3, 4).square_magnitude(), 25.0);
+    assert_eq!(a.square_distance(&b), 25.0);
+    let x = pt!(4.587, 0.);
+    assert_eq!(x.distance(&z), 4.587);
+}
+
+#[test]
+fn test_component_negation() {
+    let a: Point = (3, 0).into();
+    let b: Point = [0., 4.].into();
+    let c = Point::component(5., 53.13010235415598f64.to_radians());
+    assert_eq!(a.add(&b), c);
+
+    let a: Point = [3., 4.].into();
+    let b: Point = [4., 5.].into();
+    let subpt = b.neg();
+    let c = a.sub(&b);
+    assert_eq!(subpt, Point { x: -4., y: -5. });
+    assert_eq!(c, Point { x: -1., y: -1. });
+
+    let ar = vec![10., 150., 6.5];
+    let er = vec![280., 280., 12.8];
+    let a: Point = (&ar).into();
+    let b: Point = er[..2].into();
+
+    let pv = b.sub(&a);
+    let nv = pv.neg();
+    assert_eq!(nv, pv.kproduct(-1.));
+    let neg_a: Point = [-10f64, -150.].into();
+    assert_eq!(a.neg(), neg_a)
+}
+
+
+#[test]
+fn test_orient_dot_cross_product() {
+    let a = pt!(1.2, -4.2);
+    let b = pt!(1.2, -4.2);
+    assert_eq!(19.08, round(a.dot_product(b), 8));
+
+    let a = pt!(237, 289);
+    let b = pt!(404.25, 357.25);
+    let c = pt!(460, 380);
+    let d = pt!(297.13043478260863, 339.30434782608694);
+    let e = pt!(445.8260869565217, 350.17391304347825);
+
+    assert_eq!(c.orientation2d(a, b), 0.);
+    assert!(d.orientation2d(a, c) < 0.);
+    assert!(e.orientation2d(a, c) > 0.);
+
+    let ab = b.sub(&a);
+    let ac = c.sub(&a);
+    let ad = d.sub(&a);
+    let ae = e.sub(&a);
+    assert_eq!(ab.cross_product(ac), 0.);
+    assert!(ac.cross_product(ad) > 0.);
+    assert!(ac.cross_product(ae) < 0.);
+
+    let k = pt!(-0.887, -1.6128);
+    let u = pt!(4.55309, 1.42996);
+    let testpoints = vec![pt!(2, 2), pt!(0, 2), pt!(0, -2), pt!(2, -2), pt!(0, 0), pt!(2, 0), u, k];
+    let left = |x: f64| x < 0.;
+    let right = |x: f64| x > 0.;
+    let on = |x: f64| x.feq(0.);
+    let mut sides = vec![0.0; testpoints.len()];
+    for (i, pt) in testpoints.into_iter().enumerate() {
+        sides[i] = pt.orientation2d(k, u)
+    }
+    assert!(pt!(2,2).orientation2d(k, u) < 0.);
+    let side_out: Vec<&dyn Fn(f64) -> bool> = vec![&left, &left, &right, &right, &left, &right, &on, &on];
+    for i in 0..side_out.len() {
+        assert!(side_out[i](sides[i]))
+    }
+}
+
+#[test]
+fn test_unit_project() {
+    let a = pt!(0.88682, -1.06102);
+    let b = pt!(3.5, 1.0);
+    assert_eq!(round(a.project(b), 5), 0.56121);
+    assert_eq!(pt!(0., 0.).unit_vector(), pt!(0, 0));
+}
+
+#[test]
+fn test_direction_rev_direction() {
+    let a = pt!(0, 0);
+    let b = pt!(-1, 0);
+    let v = b.sub(&a);
+    assert_eq!(pt!(1, 1).direction(), FRAC_PI_4);//0.7853981633974483f64
+    assert_eq!(pt!(-1, 0).direction(), PI);
+    assert_eq!(v.direction(), PI);
+    assert_eq!(pt!(1, 3f64.sqrt()).direction(), 60f64.to_radians());
+    assert_eq!(pt!(0, -1).direction(), 270f64.to_radians());
+
+    let a = pt!(0, 0);
+    let b = pt!(-1, 0);
+    let v = b.sub(&a);
+    assert_eq!(Point::reverse_direction(v.direction()), 0.0);
+    assert_eq!(Point::reverse_direction(FRAC_PI_4), FRAC_PI_4 + PI);
+    assert_eq!(Point::reverse_direction(FRAC_PI_4 + PI), FRAC_PI_4);
+}
+
+#[test]
+fn test_deflection_extend() {
+    let ln0 = [pt![0, 0], pt![20, 30]];
+    let ln1 = [pt![20, 30], pt![40, 15]];
+    let v0 = ln0[1].sub(&ln0[0]);
+    let v1 = ln1[1].sub(&ln1[0]);
+
+    assert_eq!(
+        round(Point::deflection_angle(v0.direction(), v1.direction()), 10),
+        round(93.17983011986422f64.to_radians(), 10));
+    assert_eq!(
+        round(Point::deflection_angle(v0.direction(), v0.direction()), 10),
+        0f64.to_radians());
+
+    let ln1 = [pt![20, 30], pt![20, 60]];
+    let v1 = ln1[1].sub(&ln1[0]);
+    assert_eq!(
+        round(Point::deflection_angle(v0.direction(), v1.direction()), 10),
+        round(-33.690067525979806f64.to_radians(), 10), );
+
+    const PRECISION: i32 = 8;
+
+    let a2 = pt!(0.88682, -1.06102);
+    let b2 = pt!(3.5, 1);
+    let c2 = pt!(-3, 1);
+    let d2 = pt!(-1.5, -3);
+
+    let va = a2;
+    let vb = b2;
+    let vc = c2;
+    let vd = d2;
+    let vdb = b2.sub(&d2);
+    let vbc = c2.sub(&b2);
+
+    assert_eq!(round(va.direction(), PRECISION),
+               round(309.889497029295f64.to_radians(), PRECISION),
+    );
+    assert_eq!(round(vb.direction(), PRECISION),
+               round(15.945395900922854f64.to_radians(), PRECISION),
+    );
+    assert_eq!(round(vc.direction(), PRECISION),
+               round(161.565051177078f64.to_radians(), PRECISION),
+    );
+    assert_eq!(round(vd.direction(), PRECISION),
+               round(243.43494882292202f64.to_radians(), PRECISION),
+    );
+    assert_eq!(round(vdb.magnitude(), 4),
+               round(6.4031242374328485, 4),
+    );
+    assert_eq!(round(vdb.direction(), PRECISION),
+               round(38.65980825409009f64.to_radians(), PRECISION),
+    );
+    let defl_angle = 157.2855876468f64;
+    let vo = vdb.extend(
+        3.64005494464026,
+        (180.0 + defl_angle).to_radians(),
+        true,
+    );
+
+    assert_eq!(round(vo[0], PRECISION),
+               round(-vb[0], PRECISION),
+    );
+    assert_eq!(round(vo[1], PRECISION),
+               round(-vb[1], PRECISION),
+    );
+
+    // "vo by extending vdb by angle to origin"
+    // "vo by extending vdb by angle to origin"
+    let defl_angle_b = 141.34019174590992f64;
+
+    // extend to c from end
+    let vextc = vdb.extend(6.5, (180.0 + defl_angle_b).to_radians(), true);
+    assert_eq!(round(vbc[0], PRECISION), round(vextc[0], PRECISION));
+    assert_eq!(round(vbc[1], PRECISION), round(vextc[1], PRECISION));
+
+    // vextc with magnitudie extension from vdb Pnts
+    assert_eq!(round(vextc[0], PRECISION), -vextc.magnitude());
+    // vextc horizontal vector test:  extension from vdb Pnts
+    assert_eq!(round(vextc[1], PRECISION), 0.);
+
+    let c = pt!(5, 0).deflect(2., 90f64.to_radians(), true);
+    //deflection is the right hand angle
+    assert_eq!(round(c.x, PRECISION), round(0.0, PRECISION));
+    assert_eq!(round(c.y, PRECISION), round(-2., PRECISION));
+
+    let c = pt!(5, 0).deflect(2., 90f64.to_radians(), false);
+    assert_eq!(round(c.x, PRECISION), round(0.0, PRECISION));
+    assert_eq!(round(c.y, PRECISION), round(2., PRECISION));
+}
+
+#[test]
+fn test_distance_to_vector() {
+    let a = pt!(16.82295, 10.44635);
+    let b = pt!(28.99656, 15.76452);
+    let on_ab = pt!(25.32, 14.16);
+
+    let tpoints = vec![
+        pt!(30., 0.),
+        pt!(15.78786, 25.26468),
+        pt!(-2.61504, -3.09018),
+        pt!(28.85125, 27.81773),
+        a, b, on_ab, ];
+
+    let t_dists = [14.85, 13.99, 23.69, 12.05, 0.00, 0.00, 0.00];
+    let mut dists = vec![0.0; tpoints.len()];
+
+    for (i, tp) in (&tpoints).into_iter().enumerate() {
+        dists[i] = tp.distance_to_point(a, b)
+    }
+
+    assert_eq!(pt!(30., 0.).distance_to_point(a, a), a.distance(&pt!(30., 0.)));
+    assert_eq!(pt!(30., 0.).distance_to_point(b, b), b.distance(&pt!(30., 0.)));
+
+    for i in 0..tpoints.len() {
+        assert_eq!(round(dists[i], 2), round(t_dists[i], 2));
+    }
+}
